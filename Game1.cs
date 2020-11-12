b741340f184dad250081db0afba462a1e2e2d832
Saskiamei <73471215+Saskiamei@users.noreply.github.com>
2020-11-12 12:05:41 +0000

    Player falls and resets
    added the updated version of sprite class and added more to game1 and payer classes

--- Game1.cs
+++ Game1.cs
@@ -42,7 +42,7 @@ namespace JumpyThing
             whiteBox = new Texture2D(GraphicsDevice, 1, 1);
             whiteBox.SetData(new[] { Color.White });
 
-            playerSprite = new PlayerSprite(playerSheetTxr, whiteBox, new Vector2(0, 0));
+            playerSprite = new PlayerSprite(playerSheetTxr, whiteBox, new Vector2(50, 50));
 
         }
 
@@ -51,7 +51,9 @@ namespace JumpyThing
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            playerSprite.Update(gameTime);
+
+            if (playerSprite.spritePos.Y > screenSize.Y + 50) playerSprite.ResetPlayer(new Vector2(50, 50));
 
             base.Update(gameTime);
         }
--- PlayerSprite.cs
+++ PlayerSprite.cs
@@ -9,8 +9,19 @@ namespace JumpyThing
 {
     class PlayerSprite : Sprite
     {
-        public PlayerSprite(Texture2D newSpriteSheet, Texture2D newCollisionTxr, Vector2 newLocation) : base(newCollisionTxr, newSpriteSheet, newLocation)
+        bool jumping, walking, falling, jumpIsPressed;
+        float jumpSpeed = 15f;
+        float walkSpeed = 100f;
+
+        public PlayerSprite(Texture2D newSpriteSheet, Texture2D newCollisionTxr, Vector2 newLocation) 
+            : base(newSpriteSheet, newCollisionTxr, newLocation)
         {
+            //spriteOrigin = new Vector2(0.5f, 1f);
+            isColliding = true;
+            //drawCollision = true;
+            collisionInsetMin = new Vector2(0.25f, 0.3f);
+            collisionInsetMax = new Vector2(0.25f, 0f);
+
             frameTime = 0.1f;
             animations = new List<List<Rectangle>>();
 
@@ -29,7 +40,27 @@ namespace JumpyThing
 
             animations.Add(new List<Rectangle>());
             animations[3].Add(new Rectangle(0, 48, 48, 48));
+
+            jumping = false;
+            walking = false;
+            falling = true;
+            jumpIsPressed = false;
         }
 
+        public override void Update(GameTime gameTime)
+        {
+           if((falling || jumping) && spriteVelocity.Y <500f) spriteVelocity.Y += 5f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            spritePos += spriteVelocity;
+        }
+
+        public void ResetPlayer(Vector2 newPos)
+        {
+            spritePos = newPos;
+            spriteVelocity = new Vector2();
+            jumping = false;
+            walking = false;
+            falling = true;
+            jumpIsPressed = false;
+        }
     }
 }
